08ddc5dea02b027cbfc80ff4fdcb14ec90bbc261
tracer-ics2015 <tracer@njuics.org>
2015-11-01 16:07:04 +0800

    >  compile NEMU 201315011 sjy Linux sjy-NV56R 3.13.0-39-generic #66-Ubuntu SMP Tue Oct 28 13:30:27 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:07:04 up  5:44,  4 users,  load average: 0.22, 0.14, 0.14 5c42599eac91f8e7bc8dac8140fe3733c1b35954

--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -29,7 +29,7 @@ void reg_test() {
 	assert(reg_b(R_CH) == ((sample[R_ECX] >> 8) & 0xff));
 	assert(reg_b(R_DL) == (sample[R_EDX] & 0xff));
 	assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
-	printf("sample:%d cpu.eax:%d",sample[R_EAX],cpu.eax);
+	printf("sample:%d cpu.eax:%d",sample[R_ECX],cpu.ecx);
 	assert(sample[R_EAX] == cpu.eax);
 	assert(sample[R_ECX] == cpu.ecx);
 	assert(sample[R_EDX] == cpu.edx);
