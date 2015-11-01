1c398d851d26f84f41a7e86d07f6106609caa8cd
tracer-ics2015 <tracer@njuics.org>
2015-11-01 16:18:11 +0800

    >  compile NEMU 201315011 sjy Linux sjy-NV56R 3.13.0-39-generic #66-Ubuntu SMP Tue Oct 28 13:30:27 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:18:11 up  5:55,  4 users,  load average: 0.13, 0.15, 0.14 a50d25994c54b573849363c3e1f1e357621e2a9

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,6 +15,7 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
+	union{
 	union {
 		uint32_t _32;
 		uint16_t _16;
@@ -22,9 +23,10 @@ typedef struct {
 	} gpr[8];
 
 	/* Do NOT change the order of the GPRs' definitions. */
-	
+	struct {
 	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-
+	};
+	};
 	swaddr_t eip;
 
 } CPU_state;
--- nemu/src/cpu/reg.c
+++ nemu/src/cpu/reg.c
@@ -29,10 +29,8 @@ void reg_test() {
 	assert(reg_b(R_CH) == ((sample[R_ECX] >> 8) & 0xff));
 	assert(reg_b(R_DL) == (sample[R_EDX] & 0xff));
 	assert(reg_b(R_DH) == ((sample[R_EDX] >> 8) & 0xff));
-	for(i = R_EAX;i<=R_EDI;i++){
-		
-	printf("sample:%d cpu.eax:%d",sample[R_ECX],cpu.ecx);
-	}
+	
+
 	assert(sample[R_EAX] == cpu.eax);
 	assert(sample[R_ECX] == cpu.ecx);
 	assert(sample[R_EDX] == cpu.edx);
@@ -44,4 +42,3 @@ void reg_test() {
 
 	assert(eip_sample == cpu.eip);
 }
-
