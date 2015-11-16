5d46077853462e4a23465886b17ee80d37b29ee8
tracer-ics2015 <tracer@njuics.org>
2015-11-16 20:08:59 +0800

    >  compile NEMU 201315011 sjy Linux sjy-NV56R 3.13.0-39-generic #66-Ubuntu SMP Tue Oct 28 13:30:27 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:08:59 up  1:24,  1 user,  load average: 0.22, 0.22, 0.23 ebaf47abb9418c6979d9655e16bccfb922ac57f0

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -4,12 +4,13 @@
 #define NR_WP 32
 
 static WP wp_list[NR_WP];
-static WP *head, *free_;
+static WP *head;
+static WP *free_;
 
 void init_wp_list() {
 	int i;
 	for(i = 0; i < NR_WP; i ++) {
-		wp_list[i].NO = i;
+	//	wp_list[i].NO = i;
 		wp_list[i].next = &wp_list[i + 1];
 	}
 	wp_list[NR_WP - 1].next = NULL;
@@ -19,5 +20,54 @@ void init_wp_list() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(){
+	//free中删除
+	WP* temp;
+	if (free_ == NULL) {
+		Assert(0,"do not have enough watchpoint");
+	}
 
+	temp = free_;
+	free_ = free_ -> next;
+	//head中插入
+	WP* tempH = head;
+	if (head == NULL) {
+		head = temp;
+		temp->next = NULL;
+	}else	{
+		while (tempH->next!=NULL) {
+			tempH = tempH->next;
+		}
+		tempH->next = temp;
+		temp->next = NULL;
+	}
+	return temp;
+}
 
+void free_wp(WP *wp){
+	//从head中删除
+	assert(wp!=NULL);
+	WP* tempH = head;
+	if (tempH == wp) {
+		head = head->next;
+	}else{
+		while(tempH!=NULL){
+			if (wp == tempH->next) {
+				tempH->next = tempH->next->next;
+			}
+			tempH = tempH->next;
+		}
+	}
+	//将wp加入free_
+	WP* tempF = free_;
+	if (free_==NULL) {
+		free_ = wp;
+	}else{
+		while (tempF->next!=NULL) {
+			tempF = tempF->next;
+		}
+		tempF->next = wp;
+		wp->next = NULL;
+	}
+
+}
